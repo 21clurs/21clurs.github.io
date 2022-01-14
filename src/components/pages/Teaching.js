13c642846157ff963ba214ad29b65c415463e238
Clara Kim <cjk8ad@virginia.edu>
2022-01-13 19:42:53 -0500

    putting together teaching elements

--- src/components/pages/Teaching.js
+++ src/components/pages/Teaching.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import '../../App.css';
 import '../HeroSection.css';
+import MyList from '../MyList';
 
 export default function Teaching() {
     return (
     <div className='hero-container'>
         <h1>Teaching</h1>
         <p>
-            Teaching is pretty cool. I've been lucky enough to be a teaching assistant for a few classes at UVA:
+            Teaching is pretty cool. I've been lucky enough to be a teaching assistant for a few classes at UVA.
+            Here's a list in a vaguely backwards chronological manner:
         </p>
+        <MyList />
     </div>
     );
 }
