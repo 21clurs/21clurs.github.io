01c28fe19d302f0b540bdf33aa0cff4727d54dbe
Clara Kim <cjk8ad@virginia.edu>
2022-01-13 20:23:08 -0500

    adding some pages. Messing with some of the reactiveness for page getting narrower...

---
+++ src/components/pages/Design.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../App.css';
+import '../HeroSection.css';
+
+export default function Design() {
+    return (
+    <div className='hero-container'>
+        <h1>Design</h1>
+        <p>
+            This page is under construction.
+        </p>
+    </div>
+    );
+}
---
+++ src/components/pages/Illustration.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../App.css';
+import '../HeroSection.css';
+
+export default function Illustration() {
+    return (
+    <div className='hero-container'>
+        <h1>Illustration</h1>
+        <p>
+            This page is under construction.
+        </p>
+    </div>
+    );
+}
